1de3693e169a313163c9db2c2dc15844080b9231
bchen41 <bettychen41@outlook.com>
2021-12-16 16:34:33 -0500

    initial setup of controllers index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,11 @@
-const router = require('express').Router();
-const pizzaRoutes = require('./pizzaRoutes');
-const userRoutes = require('./userRoutes');
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
+router.use("/api", apiRoutes);
 
-router.use('/pizza', pizzaRoutes);
-router.use('/user', userRoutes)
-
-
-
+router.use((req, res) => {
+  res.send("<h1>Wrong Route!</h1>");
+  // to do: list of correct endpoints to use if incorrect url endpoint is entered
+});
 
 module.exports = router;
