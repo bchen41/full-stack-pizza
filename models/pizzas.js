fcb45b45b227b1f6ffd64cbc4c3e32b69cbc05a0
bchen41 <bettychen41@outlook.com>
2021-12-16 16:23:49 -0500

    refactored code for organization

--- models/pizzas.js
+++ models/pizzas.js
@@ -3,26 +3,32 @@ const sequelize = require("../config/connection");
 
 class Pizzas extends Model {}
 
-Pizzas.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  order_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  addons_ids: {
-    type: DataTypes.JSON,
-    allowNull: false,
-  },
+const options = {
   sequelize,
   timestamps: false,
   freezeTableName: true,
   underscored: true,
   modelName: "pizzas",
-});
+};
+
+Pizzas.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    order_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    addons_ids: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+  },
+  options
+);
 
 module.exports = Pizzas;
--- models/users.js
+++ models/users.js
@@ -3,33 +3,39 @@ const sequelize = require("../config/connection");
 
 class Users extends Model {}
 
-Users.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
-    },
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [8],
-    },
-  },
-  sequelize,
+const options = {
+  sequelize: sequelize,
   timestamps: false,
   freezeTableName: true,
   underscored: true,
   modelName: "users",
-});
+};
+
+Users.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  options
+);
 
 module.exports = Users;
