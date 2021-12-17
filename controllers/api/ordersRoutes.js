87bf5fa766480b1e4fa2f505d9630383f617708c
bchen41 <bettychen41@outlook.com>
2021-12-17 14:10:39 -0500

    created GET and POST methods for orders

--- controllers/api/ordersRoutes.js
+++ controllers/api/ordersRoutes.js
@@ -1,4 +1,38 @@
+const router = require("express").Router();
+const { Users, Orders, Pizzas } = require("../../models");
 
+router.get("/", async (req, res) => {
+  try {
+    const orderModels = await Orders.findAll({
+      include: [{ model: Users, attributes: ["email"] }, { model: Pizzas }],
+    });
+    res.status(200).json(orderModels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+router.get("/:id", async (req, res) => {
+  try {
+    const orderId = req.params.id;
+    const orderModel = await Orders.findByPk(orderId, {
+      include: [{ model: Users, attributes: ["email"] }, { model: Pizzas }],
+    });
+    res.status(200).json(orderModel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+router.post("/", async (req, res) => {
+  try {
+    const newOrder = req.body;
+    newOrder.created_at = new Date();
+    const orderModel = await Orders.create(newOrder);
+    res.status(200).json(orderModel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+module.exports = router;
