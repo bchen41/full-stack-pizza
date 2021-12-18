74e217b75729120341bf95a68e094bc3e31fc243
bchen41 <bettychen41@outlook.com>
2021-12-18 12:31:35 -0500

    removed name property

--- seeds/userData.js
+++ seeds/userData.js
@@ -2,17 +2,14 @@ const { Users } = require("../models");
 
 const usersData = [
   {
-    name: "Betty",
     email: "betty@email.com",
     password: "password12345",
   },
   {
-    name: "Chris",
     email: "chris@gmail.com",
     password: "password12345",
   },
   {
-    name: "Harrison",
     email: "harrison@aol.com",
     password: "password12345",
   },
