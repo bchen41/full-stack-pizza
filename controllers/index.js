e02660b70be351cbff45b9fcf733e6b3ce36ec32
bchen41 <bettychen41@outlook.com>
2021-12-20 15:36:18 -0500

    created home and signup routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
+const homeRoutes = require("./homeRoutes");
 const apiRoutes = require("./api");
+const signupRoutes = require("./signupRoutes");
 
+router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
+router.use("/signup", signupRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>");
-  // to do: list of correct endpoints to use if incorrect url endpoint is entered
 });
 
 module.exports = router;
