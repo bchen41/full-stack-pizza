7bd098807dc5f2e4163f37be1dbeeb234f4db5fd
bchen41 <bettychen41@outlook.com>
2021-12-21 10:04:19 -0500

    added pathways for buildyourown, moved withAuth code here

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,14 +2,34 @@ const router = require("express").Router();
 const path = require("path");
 const appDir = path.dirname(require.main.filename);
 const options = { root: path.join(appDir, "public") };
-const withAuth = require("../utils/auth");
+
+const withAuth = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect("/orders");
+  }
+};
+
+const withoutAuth = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    res.redirect("/");
+  } else {
+    next();
+  }
+};
+
 //homepage
 router.get("/", withAuth, async (req, res) => {
   res.sendFile("home.html", options);
 });
 
-router.get("/orders", async (req, res) => {
+router.get("/orders", withoutAuth, async (req, res) => {
   res.sendFile("orders.html", options);
+});
+
+router.get("/buildyourown", withoutAuth, async (req, res) => {
+  res.sendFile("buildyourown.html", options);
 });
 
 module.exports = router;
